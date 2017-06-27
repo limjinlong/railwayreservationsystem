18822f4b6f9d542f92678685274a593eed053f85
Jazz Ng <jazzng.js@gmail.com>
2017-06-27 20:08:01 +0800

    database changes

--- Admin_Page/Account_Management.aspx.cs
+++ Admin_Page/Account_Management.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,10 +12,29 @@ public partial class Admin_Page_Account_Management : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            SqlDataAdapter da = new SqlDataAdapter("select ID, Name, NRIC, ISNULL(ID,'') + ' - ' + ISNULL(Name,'') + ' ' + ISNULL(NRIC,'') as info from Admins", con);
+            SqlDataAdapter da2 = new SqlDataAdapter("select ID, Name, NRIC, ISNULL(ID,'') + ' - ' + ISNULL(Name,'') + ' ' + ISNULL(NRIC,'') as info from FrontDesks", con);
+            SqlDataAdapter da3 = new SqlDataAdapter("select ID, Name, NRIC, ISNULL(ID,'') + ' - ' + ISNULL(Name,'') + ' ' + ISNULL(NRIC,'') as info from Members", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            da2.Fill(dt);
+            da3.Fill(dt);
+            ddl_result.DataSource = dt;
+            ddl_result.DataTextField = "info";
+            ddl_result.DataValueField = "ID";
+            DataBind();
+        }
     }
 
     protected void btn_delete_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
     {
 
     }
